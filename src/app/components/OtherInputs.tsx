8621631ab80ceca5e8f2e2dbc6183c36115b4925
Kalyan5252 <kalyanpendem007@gmail.com>
2025-06-08 23:41:13 +0530

    ux

--- src/app/components/OtherInputs.tsx
+++ src/app/components/OtherInputs.tsx
@@ -41,54 +41,61 @@ otherInputProps) => {
     >
       <label className="text-gray-800 text-xl">{label}</label>
       {type !== 'file' ? (
-        <textarea
-          cols={30}
-          rows={5}
-          onChange={(e) => onchange(label, e.target.value)}
-          placeholder={placeholder}
-          required={required}
-          value={typeof value === 'string' ? value : ''}
-          className="border-[1px] rounded-lg resize-none border-gray-800 p-2 outline-none"
-        ></textarea>
+        <>
+          <textarea
+            cols={30}
+            rows={5}
+            onChange={(e) => onchange(label, e.target.value)}
+            placeholder={placeholder}
+            required={required}
+            maxLength={500}
+            value={typeof value === 'string' ? value : ''}
+            className="border-[1px] rounded-lg resize-none border-gray-800 p-2 outline-none"
+          ></textarea>
+          <p className="text-xs text-gray-800 -mt-3">(max 500 characters)</p>
+        </>
       ) : (
-        <div className="min-h-[40px] flex justify-between bg-transparent border-dashed border-[1px] border-gray-800 p-2 py-1 rounded-lg bg-[#1A1919] items-center outline-none text-gradient1">
-          <div className="max-h-10 max-w-10 rounded-full">
-            {((value !== '' && value) || selectedFile !== null) && (
-              <Image
-                src={
-                  selectedFile
-                    ? URL.createObjectURL(selectedFile)
-                    : `https://pub-4dba511debbc404da50bb5d141f735bc.r2.dev/business-aus/${value}`
-                }
-                alt=""
-                height={100}
-                width={100}
-                className="rounded-full mr-4 max-h-10 max-w-10"
-              />
-            )}
+        <>
+          <div className="min-h-[40px] flex justify-between bg-transparent border-dashed border-[1px] border-gray-800 p-2 py-1 rounded-lg bg-[#1A1919] items-center outline-none text-gradient1">
+            <div className="max-h-10 max-w-10 rounded-full">
+              {((value !== '' && value) || selectedFile !== null) && (
+                <Image
+                  src={
+                    selectedFile
+                      ? URL.createObjectURL(selectedFile)
+                      : `https://pub-4dba511debbc404da50bb5d141f735bc.r2.dev/business-aus/${value}`
+                  }
+                  alt=""
+                  height={100}
+                  width={100}
+                  className="rounded-full mr-4 max-h-10 max-w-10"
+                />
+              )}
+            </div>
+            <label className=" text-gray-800 overflow-hidden max-w-[29ch] mr-4 outline-none w-full">
+              {selectedFile
+                ? selectedFile.name
+                : typeof value === 'string'
+                ? value
+                : placeholder}
+            </label>
+            <button type="button" onClick={handleButtonClick}>
+              <FaUpload size={22} className="text-gray-800" />
+            </button>
+            <input
+              ref={fileInputRef}
+              type="file"
+              accept="image/*"
+              id="fileInput"
+              className="hidden"
+              onChange={(e) => {
+                setSelectedFile(e.target.files?.[0]);
+                onchange(label, e.target.files?.[0]);
+              }}
+            />
           </div>
-          <label className=" text-gray-800 overflow-hidden max-w-[29ch] mr-4 outline-none w-full">
-            {selectedFile
-              ? selectedFile.name
-              : typeof value === 'string'
-              ? value
-              : placeholder}
-          </label>
-          <button type="button" onClick={handleButtonClick}>
-            <FaUpload size={22} className="text-gray-800" />
-          </button>
-          <input
-            ref={fileInputRef}
-            type="file"
-            accept="image/*"
-            id="fileInput"
-            className="hidden"
-            onChange={(e) => {
-              setSelectedFile(e.target.files?.[0]);
-              onchange(label, e.target.files?.[0]);
-            }}
-          />
-        </div>
+          <p className="text-xs text-gray-800 -mt-3">(below 500x500)</p>
+        </>
       )}
     </div>
   );
